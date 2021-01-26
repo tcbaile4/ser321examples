5a80651655665a12a3d181aad59e30b2d74c56ce
tcbaile4 <tcbaile4@asu.edu>
2021-01-26 18:22:36 -0500

    Fix multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -200,7 +200,7 @@ class WebServer {
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
 		
-	  if (query_pairs.get("num1") != null || query_pairs.get("num2") != null) {
+	  if (query_pairs.get("num1") == null || query_pairs.get("num2") == null) {
 		 
 		builder.append("HTTP/1.1 400 Bad Request\n");
 		builder.append("Content-Type: text/html; charset=utf-8\n");
