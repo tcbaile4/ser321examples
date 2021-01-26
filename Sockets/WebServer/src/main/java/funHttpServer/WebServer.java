23e61f5b48012758c42ce91aba33e1c61742085a
tcbaile4 <tcbaile4@asu.edu>
2021-01-26 18:28:27 -0500

    Fix multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -205,12 +205,18 @@ class WebServer {
 		builder.append("HTTP/1.1 400 Bad Request\n");
 		builder.append("Content-Type: text/html; charset=utf-8\n");
 		builder.append("\n");
-		builder.append("Both parameters must be integers.");
+		builder.append("Use parameters num1 and num2.");
 	  } else {
+		  try {
           // extract required fields from parameters
           Integer num1 = Integer.parseInt(query_pairs.get("num1"));
           Integer num2 = Integer.parseInt(query_pairs.get("num2"));
-
+		  } catch (Exception e) {
+			  builder.append("HTTP/1.1 400 Bad Request\n");
+			  builder.append("Content-Type: text/html; charset=utf-8\n");
+			  builder.append("\n");
+			  builder.append("Parameters must be integers.");
+		  }
 	
 
 	          // do math
