2c8fdc24f72b36b65674fff834a42f5db994ede4
tcbaile4 <tcbaile4@asu.edu>
2021-01-26 18:30:44 -0500

    Fix multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -216,6 +216,7 @@ class WebServer {
 			  builder.append("Content-Type: text/html; charset=utf-8\n");
 			  builder.append("\n");
 			  builder.append("Parameters must be integers.");
+			  break;
 		  }
 	
 
