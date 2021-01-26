ea4fbb3aa68065b58c83289af902eb0d0ed0419b
tcbaile4 <tcbaile4@asu.edu>
2021-01-26 17:17:42 -0500

    Modify multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -204,7 +204,7 @@ class WebServer {
           Integer num1 = Integer.parseInt(query_pairs.get("num1"));
           Integer num2 = Integer.parseInt(query_pairs.get("num2"));
 
-		if (!num1.class.equals(int.class) || !num2.class.equals(int.class)) {
+		if ((num1.getClass() != Integer.class) || (num2.getClass() != Integer.class)) {
 			builder.append("HTTP/1.1 400 Bad Request\n");
 			builder.append("Content-Type: text/html; charset=utf-8\n");
 			builder.append("\n");
